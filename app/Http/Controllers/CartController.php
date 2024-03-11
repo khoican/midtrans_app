dc5d720af2cd700505d0a3c29b5f3f7b0d124e84
khoican <ibnukhoirulprasetyo@gmail.com>
2024-03-12 02:42:11 +0700

    feat: setup controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function index()
+    {
+        $carts = Cart::all();
+
+        return view('cart', compact('carts'));
+    }
+
+    public function store (Request $request)
+    {
+        $this->validate($request, [
+            'product_id' => 'required',
+            'name' => 'required',
+            'price' => 'required',
+            'quantity' => 'required',
+        ]);
+
+        Cart::create($request->all());
+
+        return redirect('cart')->with('success', 'Produk Berhasil Ditambahkan ke Keranjang');
+    }
 }
--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DetailOrder;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Order::lates();
+
+        return view('order', compact('orders'));
+    }
+
+    public function store (Request $request)
+    {
+        $this->validate($request, [
+            'product_id' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'total' => 'required',
+            'product_name' => 'required',
+            'product_price' => 'required',
+            'product_qty' => 'required',
+        ]);
+
+        Order::create([
+            'product_id' => $request->product_id,
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'total' => $request->total,
+            'status' => 'pending',
+        ]);
+
+        DetailOrder::create([
+            'order_id' => Order::latest()->first()->id,
+            'product_name' => $request->product_name,
+            'product_price' => $request->product_price,
+            'product_qty' => $request->product_qty,
+        ]);
+
+        return redirect('order')->with('success', 'Produk Berhasil Ditambahkan ke Keranjang');
+    }
 }
